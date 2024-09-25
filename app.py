5f5ff5002fa499337dfc11a0f2d67855dfbaf8ac
flinchik <116294788+flinchik@users.noreply.github.com>
2024-09-25 19:35:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello from Dockerized Flask App!")
+
+@app.route('/status')
+def status():
+    return jsonify(status="App is running", code=200)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
