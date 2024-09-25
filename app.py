141ad953f218ca5c2d2f91444de5cbc7572bb1ec
flinchik <116294788+flinchik@users.noreply.github.com>
2024-09-25 20:42:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/status')
-def status():
-    return jsonify(status="App is running", code=200)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
